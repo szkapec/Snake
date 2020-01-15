8804299cb8e6094e8374b754922f257af026b877
szkapec <szkapec999@o2.pl>
2020-01-15 17:30:08 +0100

    Updates

---
+++ precache-manifest.50444ab101311122fc0e1bc0109d0c40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9fd343e1e7d12d3f49048ad1212616",
+    "url": "/Snake/index.html"
+  },
+  {
+    "revision": "a9e36d3d9836e1f036c9",
+    "url": "/Snake/static/css/main.f954af9e.chunk.css"
+  },
+  {
+    "revision": "4e3c912ff9efa0988f67",
+    "url": "/Snake/static/js/2.1a3388d7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Snake/static/js/2.1a3388d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9e36d3d9836e1f036c9",
+    "url": "/Snake/static/js/main.f03e7da5.chunk.js"
+  },
+  {
+    "revision": "95e8180567f54fa89cb5",
+    "url": "/Snake/static/js/runtime-main.a5adae72.js"
+  }
+]);
